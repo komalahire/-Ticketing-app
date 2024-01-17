63eba77a8d1f3e86b0fe72d9c14ade55a737b464
komalahire <komala@navgurukul.org>
2024-01-17 23:08:29 +0530

    created componets and header

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-import Image from 'next/image'
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
- hey komal
-    </main>
-  )
-}
+export default Dashboard;
